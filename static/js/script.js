bdec2c77af31a5f4780c5516e36fd79dc5888453
fshan3 <fshan3@gatech.edu>
2019-10-07 01:53:34 -0400

    update with d3js.v5

--- static/js/script.js
+++ static/js/script.js
@@ -5,47 +5,46 @@ function test() {
 };
 
 function makeMap() {
-    d3.queue()
-        .defer(d3.json, 'static/json/states.json')
-        .await(loadMap);
+     var usMapDataPromise = d3.json("https://d3js.org/us-10m.v1.json");
 
-    function loadMap(error, states) {
-        console.log(states)
+    Promise.all([usMapDataPromise]).then(values => {
+        var us = values[0];
+
         //Width and height of map
         var margin = {top:50, left:50, right:50, bottom: 50};
         var height = 600 - margin.top - margin.bottom;
         var width = 1200 - margin.left - margin.right;
 
-        // D3 Projection
-        var projection = d3.geo.albersUsa()
-            .translate([width / 2, height / 2]) // translate to center of screen
-            .scale(1000); // scale things down so see entire US
-
         // Define path generator
-        var path = d3.geo.path() // path generator that will convert GeoJSON to SVG paths
-            .projection(projection); // tell path generator to use albersUsa projection
+        var path = d3.geoPath(); // path generator that will convert GeoJSON to SVG paths
 
-        // Define linear scale for output
-        var color = d3.scale.linear()
-			  .range(["rgb(213,222,217)","rgb(69,173,168)","rgb(84,36,55)","rgb(217,91,67)"]);
+        // Filter geometry objects
+        var geoMeshFilter = (a, b) => a !== b;
 
-        // Create SVG element and append map to the SVG
+        // Get GeoJson MultiLineString object representing the mesh of states
+        var geoObj = topojson.mesh(us, us.objects.states, geoMeshFilter);
+
+        // Get GeoJson features of of a specified object in a topology
+        var geoFeatures = topojson.feature(us, us.objects.states).features;
+
+        // Create SVG element and append to map
         var svg = d3.select("#map").append("svg")
             .attr("height", height + margin.top + margin.bottom)
             .attr("width", width + margin.left + margin.right)
 
-        // Append Div for tooltip to SVG
-        var div = d3.select("body")
-		    .append("div")
-    		.attr("class", "tooltip")
-    		.style("opacity", 0);
+        // Render GeoJSON object as an svg path element
+        svg.selectAll("path")
+            .data(geoFeatures)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("class", "states");
 
-        // Bind the data to the SVG and create path
-        svg.append('path')
-            .datum(topojson.feature(states, states.objects.usStates))
-            .attr('d', path)
-            .attr('class', 'states');
-    };
+        // Draw state borders
+        svg.append("path")
+            .attr("d", path(geoObj))
+            .attr("class", "state-borders");
+    });
 };
 
 
