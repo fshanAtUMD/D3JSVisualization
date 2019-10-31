91d1a1bc146b67ef90203e18bc93d171d95fb7de
fshan3 <fshan3@gatech.edu>
2019-10-30 23:00:23 -0400

    client side log

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ from psycopg2 import sql, connect
 from config import Config
 from database.db_setup import dbname, user, password
 import csv
+import time
 
 app = Flask(__name__, template_folder="template")
 app.config.from_object(Config)
@@ -21,12 +22,22 @@ def homepage():
 
 @app.route('/delay_count_by_state', methods=['GET'])
 def get_delay_count_by_state():
+    start_time = time.time()
+
     conn = connect(dbname=dbname, user=user, password=password)
     cur = conn.cursor()
+
+    connect_time = time.time() - start_time
+    app.logger.info("%.1fs for connecting to database from delay count by state API", connect_time)
+
     cur.execute("SELECT airports.state, count(*), count(distinct airports) "
                 "FROM flights INNER JOIN airports "
                 "ON flights.origin_airport = airports.iata_code "
                 "GROUP BY airports.state;")
+
+    fetch_time = time.time() - start_time
+    app.logger.info("%.1fs for fetching delay count through state data", fetch_time)
+
     records = cur.fetchall()
     cur.close()
     conn.close()
@@ -36,17 +47,31 @@ def get_delay_count_by_state():
                     'airports': records[i][2]}
                    for i in range(len(records))
                    if records[i][1] != '']
+
+    return_time = time.time() - start_time
+    app.logger.info("%ds for returning delay count by state data", return_time-fetch_time)
+
     return jsonify(records_dir), 200
 
 
 @app.route('/delay_count_by_state_week', methods=['GET'])
 def get_delay_count_by_state_week():
+    start_time = time.time()
+
     conn = connect(dbname=dbname, user=user, password=password)
     cur = conn.cursor()
+
+    connect_time = time.time() - start_time
+    app.logger.info("%.1fs for connecting to database through delay count by week API", connect_time)
+
     cur.execute("SELECT airports.state, flights.day_of_week, count(*)/count(distinct airports) "
                 "FROM flights INNER JOIN airports "
                 "ON flights.origin_airport = airports.iata_code "
                 "GROUP BY airports.state, flights.day_of_week;")
+
+    fetch_time = time.time() - start_time
+    app.logger.info("%.1fs for fetching delay count by week data", fetch_time)
+
     records = cur.fetchall()
     cur.close()
     conn.close()
@@ -55,4 +80,8 @@ def get_delay_count_by_state_week():
                     'count': records[i][2]}
                    for i in range(len(records))
                    if records[i][1] != '']
+
+    return_time = time.time() - start_time
+    app.logger.info("%.1fs for returning delay count by week data", return_time-fetch_time)
+
     return jsonify(records_dir), 200
